bff7d7230e821eb6a241872d86c59281d4cba3ad
Daniel Chamizo Mendez <115034778+Dchamizom9108@users.noreply.github.com>
2023-12-26 16:54:19 -0600

    test

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const http = require('http');
 const https = require('https');
-const app = express();
-const port = 3000;
-const httpServer = http.Server(app);
 const cors = require('cors');
 const axios = require('axios');
 const path = require('path');
 
-const rootDir = path.resolve(__dirname, '..');
+const app = express();
+const port = 3000;
+const httpServer = http.Server(app);
 
-app.use(cors());
+app.use(cors()); // Habilita el middleware de CORS
+
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
@@ -18,35 +18,31 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-
-
 app.get('/proxy', (req, res) => {
-  console.log(req.query)
+    console.log(req.query);
     const url = req.query.url;
-    // Utiliza 'https.get' para realizar la solicitud y 'pipe' para enviar la respuesta al cliente
+
     https.get(url, (responseFromOtherServer) => {
-      let rawData = '';
-      responseFromOtherServer.on('data', (chunk) => {
-        rawData += chunk;
-      });
-      responseFromOtherServer.on('end', () => {
-        try {
-          const jsonData = JSON.parse(rawData);
-          res.json(jsonData);
-        } catch (e) {
-          console.error(e.message);
-          res.status(500).json({ type: 'error', message: e.message });
-        }
-      });
+        let rawData = '';
+        responseFromOtherServer.on('data', (chunk) => {
+            rawData += chunk;
+        });
+        responseFromOtherServer.on('end', () => {
+            try {
+                const jsonData = JSON.parse(rawData);
+                res.json(jsonData);
+            } catch (e) {
+                console.error(e.message);
+                res.status(500).json({ type: 'error', message: e.message });
+            }
+        });
     }).on('error', (err) => {
         res.status(500).json({ type: 'error', message: err.message });
     });
 });
 
-
-
 app.post('/proxy_post', async (req, res) => {
-  console.log(req.body)
+    console.log(req.body);
     let data = JSON.stringify({
         "parameters": req.body.parameters
     });
